635a31feb3229164237e4eed9b002efbb22dc9d4
Paulson Bosah <paulsonbosah@gmail.com>
2025-08-05 13:46:25 +0100

    Add polling unit result

--- controllers/add-polling-unit-result.php
+++ controllers/add-polling-unit-result.php
@@ -1,10 +1,42 @@
 <?php
 
 $page_title =  "Bincom | Add Polling Unit Result";
+$polling_units = query_fetch("SELECT uniqueid, polling_unit_name FROM polling_unit");
+
+
+// Handling incoming post request
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['csrf_token'] === $_SESSION['csrf_token']) {
+    $data = [
+        'polling_unit_uniqueid' => intval(sanitize_input($_POST['polling_unit'])),
+        'party_abbreviation' => sanitize_input($_POST['party_abbreviation']),
+        'party_score' => intval(sanitize_input($_POST['party_score'])),
+        'entered_by_user' => sanitize_input($_POST['user']),
+        'date_entered' => date('Y-m-d H:i:s'),
+        'user_ip_address' => $_SERVER['REMOTE_ADDR'],
+    ];
+
+    try {
+        $sql = <<<SQL
+            INSERT INTO announced_pu_results
+                (polling_unit_uniqueid, party_abbreviation, party_score, entered_by_user, date_entered, user_ip_address)
+            VALUES
+                (:polling_unit_uniqueid, :party_abbreviation, :party_score, :entered_by_user, :date_entered, :user_ip_address)
+            SQL;
+        query_db($sql, $data);
+        redirect("add-polling-unit-result", "Result successfully added!", "success");
+    } catch (Exception $e) {
+        redirect("add-polling-unit-result", "An error occured: $e", "danger");
+    }
+}
+
+
+// Generating CSRF Token
+$csrf_token = generate_csrf_token();
 
 
 $context = [
-    'page_title'=> $page_title
+    'page_title'=> $page_title,
+    'polling_units'=> $polling_units,
 ];
 
 page_view('add-polling-unit-result', $context);
--- views/add-polling-unit-result.view.php
+++ views/add-polling-unit-result.view.php
@@ -1,24 +1,30 @@
 <h2>Add New Polling Unit Result</h2>
-<form>
+
+<?php if (isset($_SESSION['message'])): ?>
+<div class="alert alert-<?= $_SESSION['message_tag']; ?> my-2"><?= $_SESSION['message']; ?></div>
+<?php endif ?>
+
+<form method="POST">
+    <input type="hidden" name="csrf_token" value="<?=$_SESSION['csrf_token']; ?>">
     <div class="mb-3">
-        <label for="pu_id" class="form-label">Polling Unit ID</label>
-        <input type="text" class="form-control" id="pu_id" name="pu_id" required>
+        <label for="pu_id" class="form-label">Polling Unit</label>
+        <select class="form-select mb-3" name="polling_unit" required>
+            <?php foreach($context['polling_units'] as $pu): ?>
+            <option value=<?=$pu["uniqueid"]; ?>><?=$pu["polling_unit_name"]; ?></option>
+            <?php endforeach ?>
+        </select>
     </div>
     <div class="mb-3">
         <label for="party" class="form-label">Party Abbreviation</label>
-        <input type="text" class="form-control" id="party" name="party" required>
+        <input type="text" class="form-control" id="party" name="party_abbreviation" maxlength="7" required>
     </div>
     <div class="mb-3">
         <label for="score" class="form-label">Score</label>
-        <input type="number" class="form-control" id="score" name="score" required>
+        <input type="number" class="form-control" id="score" name="party_score" maxlength="12" required>
     </div>
     <div class="mb-3">
         <label for="user" class="form-label">Entered By</label>
-        <input type="text" class="form-control" id="user" name="user" required>
+        <input type="text" class="form-control" id="user" name="user" maxlength="60" required>
     </div>
     <button type="submit" class="btn btn-warning">Submit</button>
 </form>
-
-<hr class="my-4">
-<!-- Insert success or error message dynamically -->
-<div class="alert alert-success d-none">Result added successfully!</div>
